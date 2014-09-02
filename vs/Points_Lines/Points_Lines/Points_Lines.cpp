6c9d77dfd466a1ddc24981c4b81302ee56435a9b
LeiHuang <myheromyjoe@163.com>
2014-09-03 00:02:29 +0800

    add coding

--- vs/Points_Lines/Points_Lines/Points_Lines.cpp
+++ vs/Points_Lines/Points_Lines/Points_Lines.cpp
@@ -12,7 +12,25 @@ int main(int argc, char* argv)
 {
 	gltSetWorkingDirectory(argv[0]);
 	glutInit(&argc, argv);
-	glutInitDisplayMode(` )
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+
+	glutInitWindowSize(1000,1000);
+	glutCreateWindow("Square_CanMove");
+
+	GLenum err = glewInit();
+	if(err != GLEW_OK)
+	{
+		fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+		return 1;
+	}
+
+	glutReshapeFunc(ChangeSize);
+	glutDisplayFunc(RenderScene);
+	glutSpecialFunc(SpecialKeys); // listen keyboard
+
+	SetupRC();
+
+	glutMainLoop();
 	return 0;
 }
 
