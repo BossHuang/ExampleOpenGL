58f7bcde3635bfb33e7bd0569d4f622d1d47958c
LeiHuang <myheromyjoe@163.com>
2014-09-02 22:34:24 +0800

    create a project named Points_Lines

---
+++ vs/Points_Lines/Points_Lines/Points_Lines.cpp
@@ -0,0 +1,18 @@
+#include <GLTools.h>
+#include <GLShaderManager.h>
+
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else 
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+int main(int argc, char* argv)
+{
+	gltSetWorkingDirectory(argv[0]);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(` )
+	return 0;
+}
+
