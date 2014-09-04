ad4df7eebbc506d298fbc61dc6d665559e02ae63
LeiHuang <myheromyjoe@163.com>
2014-09-04 22:09:16 +0800

    DONE--Points and Lines
    Draw points and then use lines to make them connecting.
    You can use space to see all steps and the different function of drawing
    lines
    There are three function to draw different lines and one function to
    draw points.
    The points here is crrated randomly and the seed of random is current
    time.

--- vs/Points_Lines/Points_Lines/Points_Lines.cpp
+++ vs/Points_Lines/Points_Lines/Points_Lines.cpp
@@ -1,5 +1,12 @@
+//Draw points and then use lines to make them connecting.
+//You can use space to see all steps and the different function of drawing lines
+//There are three function to draw different lines and one function to draw points.
+//The points here is crrated randomly.
 #include <GLTools.h>
 #include <GLShaderManager.h>
+
+#include <stdlib.h>    //rand()
+#include <time.h>      //time()
 
 #ifdef __APPLE__
 #include <glut/glut.h>
@@ -8,14 +15,94 @@
 #include <GL/glut.h>
 #endif
 
-int main(int argc, char* argv)
+GLBatch pointsBatch;  
+GLBatch linesBatch;
+GLBatch linestripBatch;
+GLBatch lineloopBatch;
+GLShaderManager shaderManager;
+const GLint pointNumber=6;   //points number
+static int enterNumber = 0;  //control which Batch will be called
+void ChaneSize(int w, int h)  //called when window is created at first or changed
+{
+	glViewport(0,0,w,h);
+}
+void SetupPC()
+{
+	glClearColor(1.0f,1.0f,1.0f,1.0f);  //background: white
+	shaderManager.InitializeStockShaders();
+
+	GLfloat pMatrix[pointNumber][3];    // points matrix
+
+	GLfloat aRandom;
+	srand((unsigned int)time(NULL));     //random seed and the current time is the seed
+	for(int i = 0; i < pointNumber; i++)
+	{
+		aRandom = rand()/(GLfloat)RAND_MAX*2.0f-1.0f;   //create a random between -1 to 1
+		pMatrix[i][0] = aRandom;//x
+		aRandom = rand()/(GLfloat)RAND_MAX*2.0f-1.0f;
+		pMatrix[i][1] = aRandom;//y
+		pMatrix[i][2] = 0.0f;   //z
+	}
+	//Points
+	pointsBatch.Begin(GL_POINTS, pointNumber);
+	pointsBatch.CopyVertexData3f(pMatrix);
+	pointsBatch.End();
+	//Lines
+	linesBatch.Begin(GL_LINES, pointNumber);
+	linesBatch.CopyVertexData3f(pMatrix);
+	linesBatch.End();
+	//LineStrip
+	linestripBatch.Begin(GL_LINE_STRIP, pointNumber);
+	linestripBatch.CopyVertexData3f(pMatrix);
+	linestripBatch.End();
+	//LineLoop
+	lineloopBatch.Begin(GL_LINE_LOOP, pointNumber);
+	lineloopBatch.CopyVertexData3f(pMatrix);
+	lineloopBatch.End();
+}
+void RenderScene()
+{
+	//clear the window with current clearing color
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	GLfloat vRed[] = {0.0f, 0.0f, 0.0f, 1.0f};  //the color of points
+	shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
+
+	switch(enterNumber)
+	{
+	case 0:
+		glPointSize(3);
+		pointsBatch.Draw();   //draw points
+		break;
+	case 1:
+		linesBatch.Draw();    //draw Lines
+		break;
+	case 2:
+		linestripBatch.Draw();  //draw LineStrip
+		break;
+	case 3:
+		lineloopBatch.Draw();   //draw LineLoop
+		break;
+	}
+	glutSwapBuffers();    //Flush drawing commands
+}
+
+//A normal ASCII key has been pressed.
+//In this case, advance the scene when the space bar is pressed.
+void KeyPressFunc(unsigned char key, int x, int y)
+{
+	if (key == 32)   //the ASCII of space is 32
+		enterNumber = (enterNumber + 1) % 4;
+	glutPostRedisplay();  //update content of windows
+}
+
+int main(int argc, char* argv[])
 {
 	gltSetWorkingDirectory(argv[0]);
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH |GLUT_STENCIL);
 
-	glutInitWindowSize(1000,1000);
-	glutCreateWindow("Square_CanMove");
+	glutInitWindowSize(800,600);
+	glutCreateWindow("Points_Lines");
 
 	GLenum err = glewInit();
 	if(err != GLEW_OK)
@@ -24,13 +111,12 @@ int main(int argc, char* argv)
 		return 1;
 	}
 
-	glutReshapeFunc(ChangeSize);
+	glutReshapeFunc(ChaneSize);
+	glutKeyboardFunc(KeyPressFunc);
 	glutDisplayFunc(RenderScene);
-	glutSpecialFunc(SpecialKeys); // listen keyboard
-
-	SetupRC();
-
-	glutMainLoop();
+	
+	SetupPC();
+	glutMainLoop();  //main loop
 	return 0;
 }
 
