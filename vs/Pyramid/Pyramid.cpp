f4ee90fd638fde71f66676090616ba0d6be03e06
LeiHuang <myheromyjoe@163.com>
2014-09-26 23:48:59 +0800

    add outline and can rotate manual

--- vs/Pyramid/Pyramid.cpp
+++ vs/Pyramid/Pyramid.cpp
@@ -1,6 +1,6 @@
 #include <GLTools.h>
 #include <GLShaderManager.h>
-
+#include <math3d.h>
 #ifdef __APPLE__
 #include <glut/glut.h>
 #else
@@ -10,28 +10,98 @@
 
 GLShaderManager shaderManager;
 GLBatch PyramidBatch;
+
+static GLfloat Rot = 0.0f;
+M3DMatrix44f RotationMatrix;
+GLfloat VColor[] = {0.0f, 1.0f, 0.0f, 1.0f};
+
 void SetupRC()
 {
+	glEnable(GL_DEPTH_TEST);
+	
 	glClearColor(0.75f, 0.75f, 0.75f, 1.0f);
 	shaderManager.InitializeStockShaders();
 	GLfloat PyramidButtonSize = 0.1f;
-	GLfloat Pyramid[] = {PyramidButtonSize, PyramidButtonSize, 0.0f,
-	                    -PyramidButtonSize, PyramidButtonSize, 0.0f,
-	                    -PyramidButtonSize,-PyramidButtonSize, 0.0f,
-	                     PyramidButtonSize,-PyramidButtonSize, 0.0f,
-	                     0.0f,             0.0f,               0.1f};
-	PyramidBatch.Begin(GL_TRIANGLE_STRIP, 5);
+	GLfloat Pyramid[] = {0.0f, 0.0f, 0.1f,
+	                     0.1f, 0.0f, 0.0f,
+	                    -0.1f,0.1f,0.0f,
+
+						 0.0f, 0.0f, 0.1f,
+	                    -0.1f,-0.1f,0.0f,
+	                     0.1f, 0.0f, 0.0f,
+
+	                     0.0f, 0.0f, 0.1f,
+	                    -0.1f,0.1f,0.0f,
+	                    -0.1f,-0.1f,0.0f,
+
+						0.1f, 0.0f, 0.0f,
+						-0.1f,0.1f,0.0f,
+						-0.1f,-0.1f,0.0f
+	};
+	PyramidBatch.Begin(GL_TRIANGLES, 12);
 	PyramidBatch.CopyVertexData3f(Pyramid);
 	PyramidBatch.End();
+
 }
+
+
 void SceneShader()
 {
+	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-	GLfloat VColor[] = {0.25f, 0.25f, 0.25f, 1.0f};
-	shaderManager.UseStockShader(GLT_SHADER_IDENTITY , VColor);
+	
+	
+	GLfloat VvColor[] = {1.0f, 0.0f, 0.0f, 1.0f};
+	
+	
+	m3dRotationMatrix44(RotationMatrix, m3dDegToRad(Rot), 1.0f,0.0f,0.0f);
+
+	shaderManager.UseStockShader(GLT_SHADER_FLAT , RotationMatrix, VColor);
+	
 	PyramidBatch.Draw();
+	
+	glEnable(GL_POLYGON_OFFSET_LINE);
+	glPolygonOffset(-1.0f, -1.0f);
+	
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(2.5f);
+	shaderManager.UseStockShader(GLT_SHADER_FLAT , RotationMatrix, VvColor);
+	PyramidBatch.Draw();
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	
+
 	glutSwapBuffers();
+	
 }
+
+void SpecialKey(int key, int x, int y)
+{
+	if(key == GLUT_KEY_UP)
+	{
+		Rot += 5.0f;
+		m3dRotationMatrix44(RotationMatrix, m3dDegToRad(Rot), 1.0f,0.0f,0.0f);
+	}
+	if(key == GLUT_KEY_DOWN)
+	{
+		Rot -= 5.0f;
+		m3dRotationMatrix44(RotationMatrix, m3dDegToRad(Rot), 1.0f,0.0f,0.0f);
+	}
+	if(key == GLUT_KEY_LEFT)
+	{
+		Rot += 5.0f;
+		m3dRotationMatrix44(RotationMatrix, m3dDegToRad(Rot), 0.0f,1.0f,0.0f);
+	}
+	if(key == GLUT_KEY_RIGHT)
+	{
+		Rot -= 5.0f;
+		m3dRotationMatrix44(RotationMatrix, m3dDegToRad(Rot), 0.0f,1.0f,0.0f);
+	}
+	glutPostRedisplay();
+}
+//void Keyboard()
+
+
 void ChangeSize(int w, int h)
 {
 	glViewport(0, 0, w, h);
@@ -53,6 +123,8 @@ int main(int argc, char* argv[])
 	}
 	glutReshapeFunc(ChangeSize);
 	glutDisplayFunc(SceneShader);
+	glutSpecialFunc(SpecialKey);
+//	glutKeyboardFunc(Keyboard);
 
 	SetupRC();
 	glutMainLoop();
