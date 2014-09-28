9cc56789c1e399c05444d763bd28e05ce7f1d5d1
LeiHuang <myheromyjoe@163.com>
2014-09-28 20:36:55 +0800

    Undone--try to make it rotate manually   Failed

--- vs/SquareDepth/SquareDepth/SquareDepth.cpp
+++ vs/SquareDepth/SquareDepth/SquareDepth.cpp
@@ -3,6 +3,11 @@
 #include <GLShaderManager.h>
 #include <math3d.h>
 
+#include <GLFrame.h>
+#include <GLFrustum.h>
+#include <GLMatrixStack.h>
+#include <GLGeometryTransform.h>
+
 #ifdef __APPLE__
 #include <glut/glut.h>
 #else
@@ -13,17 +18,28 @@
 
 GLBatch SquareBatch;
 GLShaderManager ShaderManager;
-GLfloat SquareSize = 0.1f;
-GLfloat Square[] = {SquareSize,  SquareSize, 0.0f,
-                   -SquareSize,  SquareSize, 0.0f,
-                   -SquareSize, -SquareSize, 0.0f,
-                    SquareSize, -SquareSize, 0.0f};   //A square
-static GLfloat angle = 0.0f;  //initialize angle
+
+GLFrame             viewFrame;
+GLFrustum           viewFrustum;
+GLMatrixStack       modelViewMatrix;
+GLMatrixStack       projectionMatrix;
+GLGeometryTransform transformPipeline;
+
+//static GLfloat angle = 0.0f;  //initialize angle
 void SetupRC()
 {
 	glEnable(GL_DEPTH_TEST);   //open depth test
 	glClearColor(1.0f,1.0f,1.0f,1.0f);  //background: white
 	ShaderManager.InitializeStockShaders();
+	
+	viewFrame.MoveForward(7.0f);
+	
+	GLfloat SquareSize = 0.1f;
+	GLfloat Square[] = {SquareSize,  SquareSize, 0.0f,
+		               -SquareSize,  SquareSize, 0.0f,
+		               -SquareSize, -SquareSize, 0.0f,
+		                SquareSize, -SquareSize, 0.0f};   //A square
+
 	SquareBatch.Begin(GL_TRIANGLE_FAN, 4);
 	SquareBatch.CopyVertexData3f(Square);
 	SquareBatch.End();
@@ -31,23 +47,39 @@ void SetupRC()
 void SceneShader()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    GLfloat COLOR[] = {0.0f, 0.0f, 0.0f, 1.0f};  
+    
+	modelViewMatrix.PushMatrix(viewFrame);
 
-	M3DMatrix44f RotationMatrix;
-	angle += 1.0f;
+	GLfloat COLOR[] = {0.0f, 0.0f, 0.0f, 1.0f};  
+
+	//M3DMatrix44f RotationMatrix;
+	//angle += 1.0f;
 	// rotation matrix
 	// rotate around the vector (1.0,1.0,1.0)
-	m3dRotationMatrix44(RotationMatrix, m3dDegToRad(angle), 1.0f,1.0f,1.0f);
+	//m3dRotationMatrix44(RotationMatrix, m3dDegToRad(angle), 1.0f,1.0f,1.0f);
 
-	ShaderManager.UseStockShader(GLT_SHADER_FLAT, RotationMatrix, COLOR);
+	ShaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), COLOR);
 	SquareBatch.Draw();
+	modelViewMatrix.PopMatrix();
 	//swap buffers, and immediately refresh
 	glutSwapBuffers();
-	glutPostRedisplay();
+	//glutPostRedisplay();
 }
 void ChangeSize(int w, int h)
 {
 	glViewport(0, 0, w, h);
+}
+void SpecialKey(int key ,int x, int y)
+{
+	if(key == GLUT_KEY_UP)
+		viewFrame.RotateWorld(m3dDegToRad(-5.0),1.0f,0.0f,0.0f);
+	if(key == GLUT_KEY_DOWN)
+		viewFrame.RotateWorld(m3dDegToRad(5.0),1.0f,0.0f,0.0f);
+	if(key == GLUT_KEY_LEFT)
+		viewFrame.RotateWorld(m3dDegToRad(-5.0),0.0f,1.0f,0.0f);
+	if(key == GLUT_KEY_RIGHT)
+		viewFrame.RotateWorld(m3dDegToRad(5.0),0.0f,1.0f,0.0f);
+	glutPostRedisplay();
 }
 int main(int argc, char* argv[])
 {
@@ -66,6 +98,8 @@ int main(int argc, char* argv[])
 
 	glutReshapeFunc(ChangeSize);
 	glutDisplayFunc(SceneShader);
+
+	glutSpecialFunc(SpecialKey);
 
 	SetupRC();
 	glutMainLoop();
